5fd558d64f2ef6262e9a1d39b7a3754452812344
Alusionz <122238077+Alusionz@users.noreply.github.com>
2025-12-22 22:46:51 +0000

    [fixed] lcc.cpp

--- MMOCoreORB/src/server/zone/objects/tangible/component/lightsaber/LightsaberCrystalComponentImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/tangible/component/lightsaber/LightsaberCrystalComponentImplementation.cpp
@@ -442,7 +442,7 @@ void LightsaberCrystalComponentImplementation::tuneCrystal(CreatureObject* playe
 
 		setCustomObjectName(tuneName, true);
 
-		SceneObjectImplementation::sendDeltaUpdates();
+		broadcastObject(_this.getReferenceUnsafeStaticCast(), true);
 
 		player->notifyObservers(ObserverEventType::TUNEDCRYSTAL, _this.getReferenceUnsafeStaticCast(), 0);
 		player->sendSystemMessage("@jedi_spam:crystal_tune_success");
