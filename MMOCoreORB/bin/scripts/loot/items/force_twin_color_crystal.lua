bb158a344d8331d8c6b576864016ce80d07442d2
Alusionz <122238077+Alusionz@users.noreply.github.com>
2026-07-23 05:18:38 -0400

    Update Twin Force Crystal Cluster to use tuned color (31) for pre-P9 dual-blade lightsaber crafting

--- MMOCoreORB/bin/scripts/loot/items/force_twin_color_crystal.lua
+++ MMOCoreORB/bin/scripts/loot/items/force_twin_color_crystal.lua
@@ -1,11 +1,13 @@
 force_twin_color_crystal = {
-  minimumLevel = 0,
-  maximumLevel = -1,
-  customObjectName = "Twin Force-Crystal Cluster",
-  directObjectTemplate = "object/tangible/component/weapon/lightsaber/lightsaber_module_force_crystal.iff",
-  craftingValues = {
-    {"color",0,11,0},  -- random color
-    {50, 1, 1, 0},  -- custom marker byte key 50 = 1
-  },
+	minimumLevel = 0,
+	maximumLevel = -1,
+	customObjectName = "Twin Force Crystal Cluster",
+	directObjectTemplate = "object/tangible/component/weapon/lightsaber/lightsaber_module_force_crystal.iff",
+	craftingValues = {
+		{"color",31,31,0},  -- tuned crystal (color 31) for pre-P9 dual-blade lightsaber crafting
+	},
+	customizationStringNames = {},
+	customizationValues = {}
 }
+
 addLootItemTemplate("force_twin_color_crystal", force_twin_color_crystal)
