89ef9bfaed5ed6787e37252296780fbc68975917
Alusionz <122238077+Alusionz@users.noreply.github.com>
2026-07-23 06:10:50 -0400

    Pre-P9 lightsaber schematics: remove refined crystal packs, use force crystals, add progressive Gen N requires Gen N-1 saber (one-handed Gen2-4 first)

--- MMOCoreORB/bin/scripts/object/draft_schematic/weapon/lightsaber/lightsaber_one_hand_gen2.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/weapon/lightsaber/lightsaber_one_hand_gen2.lua
@@ -1,78 +1,37 @@
 --Copyright (C) 2010 <SWGEmu>
-
-
 --This File is part of Core3.
-
---This program is free software; you can redistribute 
---it and/or modify it under the terms of the GNU Lesser 
---General Public License as published by the Free Software
---Foundation; either version 2 of the License, 
---or (at your option) any later version.
-
---This program is distributed in the hope that it will be useful, 
---but WITHOUT ANY WARRANTY; without even the implied warranty of 
---MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
---See the GNU Lesser General Public License for
---more details.
-
---You should have received a copy of the GNU Lesser General 
---Public License along with this program; if not, write to
---the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-
---Linking Engine3 statically or dynamically with other modules 
---is making a combined work based on Engine3. 
---Thus, the terms and conditions of the GNU Lesser General Public License 
---cover the whole combination.
-
---In addition, as a special exception, the copyright holders of Engine3 
---give you permission to combine Engine3 program with free software 
---programs or libraries that are released under the GNU LGPL and with 
---code included in the standard release of Core3 under the GNU LGPL 
---license (or modified versions of such code, with unchanged license). 
---You may copy and distribute such a system following the terms of the 
---GNU LGPL for Engine3 and the licenses of the other code concerned, 
---provided that you include the source code of that other code when 
---and as the GNU LGPL requires distribution of source code.
-
---Note that people who make modified versions of Engine3 are not obligated 
---to grant this special exception for their modified versions; 
---it is their choice whether to do so. The GNU Lesser General Public License 
---gives permission to release a modified version without this exception; 
---this exception also makes it possible to release a modified version 
-
 
 object_draft_schematic_weapon_lightsaber_lightsaber_one_hand_gen2 = object_draft_schematic_weapon_lightsaber_shared_lightsaber_one_hand_gen2:new {
 
    templateType = DRAFTSCHEMATIC,
 
    customObjectName = "Second Generation Lightsaber",
-   noCrystalContainer = true;
-   noCrystalContainer = true;
+   noCrystalContainer = true,
 
-   craftingToolTab = 2048, -- (See DraftSchematicObjectTemplate.h)
-   complexity = 17, 
-   size = 1, 
+   craftingToolTab = 2048,
+   complexity = 17,
+   size = 1,
    factoryCrateType = "object/factory/factory_crate_weapon.iff",
-   
-   xpType = "jedi_general", 
-   xp = 0, 
 
-   assemblySkill = "jedi_saber_assembly", 
-   experimentingSkill = "jedi_saber_experimentation", 
-   customizationSkill = "jedi_customization", 
+   xpType = "jedi_general",
+   xp = 0,
+
+   assemblySkill = "jedi_saber_assembly",
+   experimentingSkill = "jedi_saber_experimentation",
+   customizationSkill = "jedi_customization",
    factoryCrateSize = 0,
 
    customizationOptions = {1},
    customizationStringNames = {"/private/index_no_container"},
    customizationDefaults = {1},
 
-   ingredientTemplateNames = {"craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n"},
-   ingredientTitleNames = {"emitter_shroud", "force_crystal", "activator", "handgrip", "power_field_insulator", "energizers"},
-   ingredientSlotType = {0, 1, 0, 0, 0, 0},
-   resourceTypes = {"metal", "object/tangible/component/weapon/lightsaber/shared_lightsaber_module_force_crystal.iff", "metal_nonferrous", "petrochem_inert", "gas_inert_known", "metal_nonferrous"},
-   resourceQuantities = {25, 1, 18, 20, 22, 20},
-   contribution = {100, 200, 100, 100, 100, 100},
-
+   -- Pre-P9: Gen2 requires materials + force crystal + Gen1 lightsaber
+   ingredientTemplateNames = {"craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n", "craft_weapon_ingredients_n"},
+   ingredientTitleNames = {"emitter_shroud", "force_crystal", "activator", "handgrip", "power_field_insulator", "energizers", "previous_generation_lightsaber"},
+   ingredientSlotType = {0, 1, 0, 0, 0, 0, 1},
+   resourceTypes = {"metal", "object/tangible/component/weapon/lightsaber/shared_lightsaber_module_force_crystal.iff", "metal_nonferrous", "petrochem_inert", "gas_inert_known", "metal_nonferrous", "object/weapon/melee/sword/crafted_saber/shared_sword_lightsaber_one_handed_gen1.iff"},
+   resourceQuantities = {25, 1, 18, 20, 22, 20, 1},
+   contribution = {100, 200, 100, 100, 100, 100, 100},
 
    targetTemplate = "object/weapon/melee/sword/crafted_saber/sword_lightsaber_one_handed_gen2_pre9.iff",
 
