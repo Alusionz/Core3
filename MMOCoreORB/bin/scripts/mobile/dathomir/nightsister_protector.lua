e3d6f6b7225e6870fa02b2bc86f06076ae0888af
Alusionz <122238077+Alusionz@users.noreply.github.com>
2026-08-02 21:52:22 -0400

    Pre-P9: ~70% damage cut, armor 2->1, soften resists on Nightsister Protector

--- MMOCoreORB/bin/scripts/mobile/dathomir/nightsister_protector.lua
+++ MMOCoreORB/bin/scripts/mobile/dathomir/nightsister_protector.lua
@@ -7,13 +7,13 @@ nightsister_protector = Creature:new {
 	faction = "nightsister",
 	level = 131,
 	chanceHit = 4.75,
-	damageMin = 770,
-	damageMax = 1250,
+	damageMin = 231,
+	damageMax = 375,
 	baseXp = 12424,
 	baseHAM = 30000,
 	baseHAMmax = 38000,
-	armor = 2,
-	resists = {15,200,15,200,200,200,200,200,-1},
+	armor = 1,
+	resists = {15,100,15,100,100,100,100,100,-1},
 	meatType = "",
 	meatAmount = 0,
 	hideType = "",
@@ -37,14 +37,9 @@ nightsister_protector = Creature:new {
 		}
 	},
 
-	-- Primary and secondary weapon should be different types (rifle/carbine, carbine/pistol, rifle/unarmed, etc)
-	-- Unarmed should be put on secondary unless the mobile doesn't use weapons, in which case "unarmed" should be put primary and "none" as secondary
 	primaryWeapon = "force_sword",
 	secondaryWeapon = "force_sword_ranged",
 	conversationTemplate = "",
-
-	-- primaryAttacks and secondaryAttacks should be separate skill groups specific to the weapon type listed in primaryWeapon and secondaryWeapon
-	-- Use merge() to merge groups in creatureskills.lua together. If a weapon is set to "none", set the attacks variable to empty brackets
 	primaryAttacks = merge(fencermaster,swordsmanmaster,tkamid,brawlermaster,pikemanmaster,forcewielder),
 	secondaryAttacks = forcewielder
 }
