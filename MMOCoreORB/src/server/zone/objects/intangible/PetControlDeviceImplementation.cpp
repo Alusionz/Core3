0ba145af3f3b63484e6858f3d4050c6bb1c966fe
Alusionz <122238077+Alusionz@users.noreply.github.com>
2026-08-02 21:05:59 -0400

    Simplify max pet checks for faction and droid pets
    Edit #2 AT-ST change attempt

--- MMOCoreORB/src/server/zone/objects/intangible/PetControlDeviceImplementation.cpp
+++ MMOCoreORB/src/server/zone/objects/intangible/PetControlDeviceImplementation.cpp
@@ -266,13 +266,13 @@ void PetControlDeviceImplementation::callObject(CreatureObject* player, bool ini
 					return;
 				}
 			} else if (object->isCreature() && petType == PetManager::FACTIONPET) {
+				// AT-ST and other creature-type faction pets count toward the normal maxPets = 3 limit
 				const CreatureTemplate* activePetTemplate = object->getCreatureTemplate();
-				const CreatureTemplate* callingPetTemplate = pet->getCreatureTemplate();
 
-				if (activePetTemplate == nullptr || callingPetTemplate == nullptr || activePetTemplate->getTemplateName() != "at_st")
+				if (activePetTemplate == nullptr)
 					continue;
 
-				if (++currentlySpawned >= maxPets || (activePetTemplate->getTemplateName() == "at_st" && callingPetTemplate->getTemplateName() == "at_st")) {
+				if (++currentlySpawned >= maxPets) {
 					player->sendSystemMessage("@pet/pet_menu:at_max"); // You already have the maximum number of pets of this type that you can call.
 					return;
 				}
