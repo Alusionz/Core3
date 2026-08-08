79bc81bc2f15aac16954684205da3fb056daf1e1
Alusionz <122238077+Alusionz@users.noreply.github.com>
2026-08-08 07:37:46 -0400

    Pre-P9: twin force crystal clusters roll random blade color 0-11 for client appearance

--- MMOCoreORB/bin/scripts/loot/items/force_twin_color_crystal.lua
+++ MMOCoreORB/bin/scripts/loot/items/force_twin_color_crystal.lua
@@ -4,7 +4,7 @@ force_twin_color_crystal = {
 	customObjectName = "Twin Force Crystal Cluster",
 	directObjectTemplate = "object/tangible/component/weapon/lightsaber/lightsaber_module_force_crystal.iff",
 	craftingValues = {
-		{"color",31,31,0},  -- tuned crystal (color 31) for pre-P9 dual-blade lightsaber crafting
+		{"color",0,11,0},  -- Pre-P9: random color for client appearance (same as color crystals)
 	},
 	customizationStringNames = {},
 	customizationValues = {}
